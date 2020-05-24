ac0b9d94be0128c515e6ca7529625ea74d761ecd
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-24 21:09:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
   reverse_sentence = []
-sentence.each do |inreverse|
+words.each do |word|
   new_array << inreverse.reverse
 end
   new_array
