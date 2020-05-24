e77d310a36021db41f51b51be6c6f4e7dbe085a6
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-24 21:02:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence = "Hello there, and how are you?")
 new_array = []
 sentence.each do |inreverse|
-  new_array << sentence.reverse 
-
+  new_array << inreverse.reverse 
 end
   new_array
 end
